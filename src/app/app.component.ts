9774b7aa7add93821fbc924448663418282bd2a8
Josef Drda <drda.josef@gmail.com>
2018-11-03 13:20:23 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testpwa';
+  public code: number = null;
+  public progress;
+  public percent: number = null;
+  protected coderoot = 7849000;
+
+  start() {
+    this.code = this.getCode();
+    this.getProgress();
+    this.progress = setInterval(() => {
+      this.getProgress();
+    }, 500);
+  }
+
+  getCode() {
+    const date = this.prepareDate();
+
+    const code = this.coderoot +
+      (parseInt(date.day.slice(1)) * 100) +
+      (parseInt(date.hour.slice(1)) * 10) +
+      (parseInt(date.minute.slice(1)));
+    return code;
+  }
+
+  refreshCode() {
+    this.code = this.getCode();
+  }
+
+  getProgress() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const percent = 100 - Math.ceil((( seconds / 60 ) * 100));
+
+    this.percent = percent;
+
+    if (percent <= 100 && percent >= 99) {
+      this.refreshCode();
+    }
+  }
+
+  prepareDate() {
+    const now = new Date();
+    const day = ('0' + now.getDate()).slice(-2);
+    const hour = ('0' + (now.getHours())).slice(-2);
+    const minute = ('0' + (now.getMinutes())).slice(-2);
+    const date = {'day': day, 'hour': hour, 'minute': minute};
+    return date;
+
+  }
 }
